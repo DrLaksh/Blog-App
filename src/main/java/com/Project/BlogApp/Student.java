5f6efa230b925089c1725cc2d51d3ead44ce13fe
DrLaksh <lakshyaacharya.engineer@gmail.com>
2025-02-07 00:23:08 +0530

    First Update

---
+++ src/main/java/com/Project/BlogApp/Student.java
@@ -0,0 +1,5 @@
+package com.Project.BlogApp;
+
+public class Student {
+    
+}
---
+++ src/test/java/com/Project/BlogApp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.BlogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
